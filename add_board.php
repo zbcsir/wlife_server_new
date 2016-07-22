e256e8072592bda695be4a2eb5caed04cd1722d7
jlshix <jlshix@163.com>
2016-07-22 15:56:16 +0800

    新增家庭留言板

--- add_board.php
+++ add_board.php
@@ -10,3 +10,25 @@
  * 描述: insert into board
  * 返回: 成功返回 success() 失败返回相应错误码与描述 参见data.php
  */
+require_once 'data.php';
+if (isset($_POST['gate']) && isset($_POST['name']) && isset($_POST['content']) ) {
+    $gate = $_POST['gate'];
+    $name = $_POST['name'];
+    $content = $_POST['content'];
+
+    $pdo = connect();
+    $sql = "INSERT INTO `board`(`gate`, `name`, `content`)
+                  VALUES ('{$gate}', '{$name}', '{$content}')";
+    $pdo->beginTransaction();
+    $stmt = $pdo->query($sql);
+    $row = $stmt->rowCount();
+    if ($row == 1) {
+        $pdo->commit();
+        echo success();
+    } else {
+        $pdo->rollBack();
+        echo fail();
+    }
+} else {
+    echo no_para();
+}
--- get_board.php
+++ get_board.php
@@ -11,3 +11,22 @@
  * 返回: 成功返回 {code des info} code状态码 des描述 info为内容 直接ba查询结果转为json放入此字段即可
  *      失败返回相应错误码与描述 参见data.php
  */
+require_once 'data.php';
+if (isset($_POST['gate'])) {
+    $gate = $_POST['gate'];
+    $pdo =connect();
+    $sql = "SELECT * FROM `board` WHERE `gate`='{$gate}'";
+    $stmt = $pdo->query($sql);
+    $info = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $res = array(
+        "code" => "1",
+        "des" => "success",
+        "info" => $info
+    );
+
+    echo json_encode($res);
+
+} else {
+    echo no_para();
+}
